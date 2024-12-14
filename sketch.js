7c972102dcd5fc6c987d726cad6214407393ec43
notsonya <117752607+notsonya@users.noreply.github.com>
2024-12-14 23:23:13 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,62 +1,65 @@
-let picture;
 let scale = 10;
 let pictureWidth;
 let pictureHeight;
 let tilesX;
 let tilesY;
 
-
-function preload(){
+function preload() {
   picture = loadImage("meow.png");
-
 }
 
 function setup() {
   pictureHeight = picture.height;
   pictureWidth = picture.width;
 
-  createCanvas (pictureWidth, pictureHeight);
+  createCanvas(pictureWidth, pictureHeight);
 
-  tilesX = pictureWidth/scale;
-  tilesY = pictureHeight/scale;
-  tileWidth = width/tilesX;
-  tileHeight = height/tilesY;
+  tilesX = pictureWidth / scale;
+  tilesY = pictureHeight / scale;
+  tileWidth = width / tilesX;
+  tileHeight = height / tilesY;
   picture.resize(tilesX, tilesY);
 
-  textAlign (CENTER);
-  textSize (10);
+  textAlign(CENTER);
+  textSize(10);
 }
 
-function draw () {
-  background (250);
+function draw() {
+  background(250);
 
-  //image(picture, 0, 0, width, height)
-
-  for (let x = 0; x < tilesX; x++){
+  for (let x = 0; x < tilesX; x++) {
     for (let y = 0; y < tilesY; y++) {
-      let pixel = picture.get(x,y);
+      let pixel = picture.get(x, y);
       let color = brightness(pixel);
 
-      let char = "meow";
+      let tileCenterX = x * tileWidth + tileWidth / 2;
+      let tileCenterY = y * tileHeight + tileHeight / 2;
 
-      if (color < 80){
-        char = "✧✧"
+      let distance = dist(mouseX, mouseY, tileCenterX, tileCenterY);
+
+      let maxDistance = dist(0, 0, width / 2, height / 2);
+      let alpha = map(distance, 0, maxDistance, 255, 0);
+      alpha = constrain(alpha, 0, 255);
+
+      let char = "･ﾟ";
+
+      if (color < 80) {
+        char = "✧✧";
+      }
+      if (color < 60) {
+        char = "*";
+      }
+      if (color < 40) {
+        char = "░";
+      }
+      if (color < 20) {
+        char = "▓";
       }
 
-      if (color < 60){
-        char = "*"
-      }
-
-      if (color < 40){
-        char = "░"
-      }
-      if (color < 20){
-        char = "▓"
-      }
-      text(char, x * tileWidth, y * tileHeight);
-
-      fill(color * mouseY);
-      // rect(x * tileWidth, y * tileHeight, tileWidth, tileHeight);
+      fill(0, alpha); // Adjust transparency based on distance
+      noStroke();
+      text(char, tileCenterX, tileCenterY);
     }
   }
 }
+
